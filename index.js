86eb4d528a264301e879af70836c560f0e0dc8b5
Ali awsat <aliawsat278@gmail.com>
2024-06-15 16:56:23 +0430

    first commit

--- index.js
+++ index.js
@@ -13,20 +13,3 @@ app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
 
-// replace the value below with the Telegram token you receive from @BotFather
-const token = '5959632379:AAGuCMDdqhOUHadISS2n79Iw8dTjzgYYvMs';
-
-const bot = new TelegramBot(token, { polling: true });
-
-bot.on("message", async (msg) => {
-    const chatId = msg.chat.id;
-    const userInput = msg.text;
-    const msg_id = msg.message_id;
-
-    sent_message = await bot.sendMessage(chatId, userInput, {
-        reply_to_message_id: msg_id,
-    });
-
-});
-
-module.exports = app;
